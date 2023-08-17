0d66748cc5e27a7873d1ec5f470839d0a8d16521
nadin869 <nady86.86@mail.ru>
2023-08-18 00:23:23 +0300

    Добавлен пакет dm_api_search_async

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+REQUIRES = [
+    'betterproto',
+    'grpclib',
+    'google',
+]
+setup(
+    name='dm_api_search_async',
+    version='0.0.1',
+    packages=['dm_api_search_async'],
+    url='https://github.com/NadezhdaDudnik/dm_api_search_async.git',
+    license='MIT',
+    author='Nadezhda Dudnik',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='dm_api_search_async'
+)
